bfbc6d0c847a52eaa111feaeabcc30d0c3606691
Jack Wu <jackwuchn@outlook.com>
2022-12-12 13:55:49 +0800

    login

--- pages/games/profile.js
+++ pages/games/profile.js
@@ -1,6 +1,6 @@
 // pages/games/profie.js
 const app = getApp()
-
+import event from '@codesmiths/event';
 Page({
 
   /**
@@ -23,26 +23,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    
+    // this.setData({id: options.id})
+    // if (getApp().globalData.header) {
+    //   this.loginLoad();
+    // } else {
+    //   event.on('tokenReady', this, this.loginLoad)
+    // }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-    const page = this;
-    page.setData({
-      user: app.globalData.user,
-      userId: app.globalData.userId
-    })
-    this.loadBookings();
+  loginLoad() {
+    const page = this
     wx.request({
       url: `${app.globalData.baseUrl}/users/${app.globalData.userId}/likes`,
       method: "GET",
@@ -65,6 +55,26 @@ Page({
         page.setData({activeTab: "bookings"})
       }
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    const page = this;
+    // this.loadBookings();
+    if (getApp().globalData.header) {
+      this.loginLoad();
+    } else {
+      event.on('tokenReady', this, this.loginLoad)
+    }
   },
 
   // loadBookings() {
