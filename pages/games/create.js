97ffc7def157c1f21454c7312e226f937a89236f
Jack Wu <jackwuchn@outlook.com>
2022-12-13 13:14:05 +0800

    createpage

--- pages/games/create.js
+++ pages/games/create.js
@@ -1,13 +1,50 @@
 // pages/games/create.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userName: "",
+    userDescription: "",
+    array: ['ps5', 'xbox', 'switch'],
+    objectArray: [
+      {
+        id: 0,
+        name: 'ps5'
+      },
+      {
+        id: 1,
+        name: 'xbox'
+      },
+      {
+        id: 2,
+        name: 'switch'
+      }
+    ],
+    index: 0
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
   },
 
+  Upload(e) {
+    const data = { name: e.detail.value.name, description: e.detail.value.description, platform: this.data.array[e.detail.value.console], user_id: app.globalData.userId}
+    console.log(data)
+    wx.request({
+      url: `${app.globalData.baseUrl}/products`,
+      method:"POST",
+      header: getApp().globalData.header,
+      data: data,
+      success(res) {
+        console.log("success", res)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
